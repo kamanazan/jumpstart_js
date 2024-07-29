2bc563e07e57ed23b588adfc3ee7ef5bf842040a
Muhammad Fauzan Rosyadi <fauzan.rosyadi@gmail.com>
2024-07-29 11:55:07 +0700

    1.2 can mark todo as complete

--- exercise1.2-fauzan_rosyadi/script.js
+++ exercise1.2-fauzan_rosyadi/script.js
@@ -5,11 +5,23 @@ function addTodo() {
   // TODO: Implement the addTodo function
   const newTodo = todoInput.value;
   if (newTodo){
+    // const idTodo = `todo-${new Date().getTime()}`
     const newTodoList = document.createElement('li');
+    // newTodoList.setAttribute('id', idTodo)
     newTodoList.textContent = newTodo;
+    newTodoList.onclick = markComplete;
     todoList.append(newTodoList);
+    todoInput.value = '';
   }
 }
 
-// TODO: Implement a function to mark tasks as complete
+function markComplete(event) {
+ console.log(event);
+ if (event.target.classList.contains('todo-complete')) {
+   event.target.classList.remove('todo-complete');
+ } else {
+   event.target.classList.add('todo-complete');
+ }
+}
+
 // TODO: Implement a function to remove tasks

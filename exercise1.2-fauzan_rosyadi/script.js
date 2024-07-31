cddb1381c9a6a268b349e9d6f6b044541e8c60c2
Muhammad Fauzan Rosyadi <fauzan.rosyadi@gmail.com>
2024-07-31 16:27:49 +0700

    bisa edit list

--- exercise1.2-fauzan_rosyadi/script.js
+++ exercise1.2-fauzan_rosyadi/script.js
@@ -1,27 +1,84 @@
 const todoInput = document.querySelector('#todoInput');
 const todoList = document.querySelector('#todoList');
+const template = document.querySelector('#contentTodo');
 
 function addTodo() {
-  // TODO: Implement the addTodo function
   const newTodo = todoInput.value;
+  console.log(newTodo)
   if (newTodo){
-    // const idTodo = `todo-${new Date().getTime()}`
-    const newTodoList = document.createElement('li');
-    // newTodoList.setAttribute('id', idTodo)
-    newTodoList.textContent = newTodo;
-    newTodoList.onclick = markComplete;
-    todoList.append(newTodoList);
+    const newTodoList = template.content.cloneNode(true)  //document.createElement('li');
+    const span = newTodoList.querySelector('span')
+    const input = newTodoList.querySelector('input');
+    const saveBtn = newTodoList.querySelector('#saveButton');
+    const editBtn = newTodoList.querySelector('#editButton');
+    const completeBtn = newTodoList.querySelector('#completeButton');
+    const removeBtn = newTodoList.querySelector('#removeButton');
+    input.classList.add('hide');
+    saveBtn.classList.add('hide');
+    span.textContent = newTodo;
+    completeBtn.onclick = markComplete;
+    removeBtn.onclick = removeTodo;
+    editBtn.onclick = editTodo;
+    saveBtn.onclick = saveTodo;
+    todoList.appendChild(newTodoList);
     todoInput.value = '';
   }
 }
 
 function markComplete(event) {
- console.log(event);
- if (event.target.classList.contains('todo-complete')) {
-   event.target.classList.remove('todo-complete');
- } else {
-   event.target.classList.add('todo-complete');
- }
+  const button = event.target;
+  const content = button.parentNode.querySelector('span');
+  if (content.classList.contains('todo-complete')) {
+    content.classList.remove('todo-complete');
+    button.textContent = 'mark complete'
+  } else {
+    content.classList.add('todo-complete');
+    button.textContent = 'unmark';
+  }
 }
 
-// TODO: Implement a function to remove tasks
+
+function removeTodo(event) {
+  const button = event.target;
+  const parent = button.parentNode;
+  parent.remove();
+}
+
+function editTodo(event) {
+  const button = event.target;
+  const parent = button.parentNode;
+  const span = parent.querySelector('span')
+  const input = parent.querySelector('input');
+  const saveBtn = parent.querySelector('#saveButton');
+  const editBtn = parent.querySelector('#editButton');
+  const completeBtn = parent.querySelector('#completeButton');
+  const removeBtn = parent.querySelector('#removeButton');
+  span.classList.add('hide');
+  input.classList.remove('hide');
+  saveBtn.classList.remove('hide');
+  editBtn.classList.add('hide');
+  completeBtn.classList.add('hide');
+  removeBtn.classList.add('hide');
+  input.value = span.textContent;
+}
+
+function saveTodo(event) {
+  const button = event.target;
+  const parent = button.parentNode;
+  const span = parent.querySelector('span')
+  const input = parent.querySelector('input');
+  const saveBtn = parent.querySelector('#saveButton');
+  const editBtn = parent.querySelector('#editButton');
+  const completeBtn = parent.querySelector('#completeButton');
+  const removeBtn = parent.querySelector('#removeButton');
+  const newVal = input.value;
+  if (newVal) {
+    span.textContent = input.value;
+  }
+  span.classList.remove('hide');
+  input.classList.add('hide');
+  saveBtn.classList.add('hide');
+  editBtn.classList.remove('hide');
+  completeBtn.classList.remove('hide');
+  removeBtn.classList.remove('hide');
+}
